709017cf3ca3d7c09348b51ae6314179d8bb0ef7
David <whoisdavidd@hotmail.com>
2024-09-19 17:08:43 +0800

    added swr

--- worknest/next.config.mjs
+++ worknest/next.config.mjs
@@ -1,11 +1,4 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
