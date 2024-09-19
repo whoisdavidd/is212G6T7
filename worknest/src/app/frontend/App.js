81caaeefec887d6c10214872f349011661bdd63a
jcjc13 <javanchok13@hotmail.com>
2024-09-19 14:44:33 +0800

    Skeleton of Staff's view of own schedule
    Skeleton for staff's view of own schedule

---
+++ worknest/src/app/frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StaffCalendarPage from './components/StaffCalendarPage';
+
+function App() {
+  return (
+    <div className="App">
+      <StaffCalendarPage />
+    </div>
+  );
+}
+
+export default App;
