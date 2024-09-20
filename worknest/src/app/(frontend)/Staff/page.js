4e379a736e5afbc5032b425a6c8e7576a97878d3
jcjc13 <javanchok13@hotmail.com>
2024-09-20 14:22:30 +0800

    added another test calendar for testing

--- worknest/src/app/(frontend)/Staff/page.js
+++ worknest/src/app/(frontend)/Staff/page.js
@@ -2,12 +2,14 @@
 import React, { useState } from 'react';
 import StaffCalendarPage from '../components/StaffCalendarPage';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
+import Test from '../components/Test';
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
       <StaffCalendarPage />
+      <Test />
 
     </div>
   );
---
+++ worknest/src/app/(frontend)/components/Test.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {
+  ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject
+} from '@syncfusion/ej2-react-schedule';
+ 
+const Test = () => {
+  const data = [{
+    Id: 1,
+    Subject: 'Scrum Meeting',
+    Location: 'Office',
+    StartTime: new Date(2024, 8, 20, 9, 30),
+    EndTime: new Date(2024, 8, 20, 10, 30),
+    RecurrenceRule: 'FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR;INTERVAL=1'
+  }];
+ 
+  return (
+    <div>
+      <ScheduleComponent height='650px' eventSettings={{dataSource:data}}>
+        <ViewsDirective>
+          <ViewDirective option='Day' />
+          <ViewDirective option='Week' />
+          <ViewDirective option='WorkWeek' />
+          <ViewDirective option='Month' />
+          <ViewDirective option='Agenda' />
+        </ViewsDirective>
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      </ScheduleComponent>
+      <link href="https://cdn.syncfusion.com/ej2/material.css" rel="stylesheet" type="text/css"/>
+    </div>
+  );
+}
+
+export default Test;
