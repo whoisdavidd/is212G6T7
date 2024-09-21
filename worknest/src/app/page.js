aeac82cbd70bba505073545e0b0dc265ff37541b
David <whoisdavidd@hotmail.com>
2024-09-21 11:45:17 +0800

    Created the login page
    - Now we have a login page!

--- worknest/src/app/page.js
+++ worknest/src/app/page.js
@@ -1,16 +1,40 @@
+import React from "react";
 
-"use client";
-import React, { useState } from 'react';
-import StaffCalendarPage from './(frontend)/components/StaffCalendarPage';
-import ResponsiveAppBar from './(frontend)/components/ResponsiveAppBar';
-
-function App() {
+export default function HomePage() {
   return (
-    <div className="App">
-      <StaffCalendarPage />
-      <ResponsiveAppBar />
+    <div className="relative h-screen flex items-center">
+      {/* Background Image */}
+    
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: 'url("/background.svg")' }}
+      ></div>
+
+      <div className="absolute top-1/3 left-1/3 transform -translate-x-1/2 -translate-y-1/2 text-center font-extrabold text-[90px] leading-[22px] text-[#21005d] w-[463.8px] h-[119.3px]">
+        WorkNest
+      </div>
+
+      <div className="relative z-10 w-auto max-w-md ml-auto mr-16 flex flex-col justify-center p-8 bg-white bg-opacity-90 shadow-lg rounded-lg">
+        <h1 className="text-4xl font-bold text-center mb-6">Welcome Back</h1>
+        <form className="w-full space-y-4">
+          <input
+            type="text"
+            placeholder="Username"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
