a7729ff6b0e260a1e9111bb04580eb2e5f682874
Izz Danial Sharudin <danials.2022@scis.smu.edu.sg>
2024-09-29 17:48:11 +0800

    Replace back the StaffCalendar.jsx

--- worknest/src/app/backend/employee/employee.py
+++ worknest/src/app/backend/employee/employee.py
@@ -13,7 +13,7 @@ db_url = os.getenv("DATABASE_URL")
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Javanchok13@localhost:5432/employee'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/employee'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
