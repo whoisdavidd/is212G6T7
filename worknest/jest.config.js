b8ed84f68150313ad4d8e2c23e3ad9f271256b79
David <whoisdavidd@hotmail.com>
2024-10-24 16:39:00 +0800

    update the jest

--- worknest/jest.config.js
+++ worknest/jest.config.js
@@ -1,20 +1,22 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  dir: './', // This tells Jest to use the Next.js root directory.
+  dir: './', // Tells Jest to use the Next.js root directory
 });
 
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Adjusted path, if needed
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
-  // Correct testMatch to look for .test.mjs files
-  testMatch: ['<rootDir>/src/app/test/*.test.mjs'], // Adjusted to match .test.mjs files
+  // Ensure Jest matches .test.mjs files
+  testMatch: ['<rootDir>/src/app/test/*.test.mjs'],
   transform: {
-    '^.+\\.mjs$': 'babel-jest', // Transform .mjs files using Babel
+    // Use babel-jest to transform .mjs files
+    '^.+\\.mjs$': 'babel-jest',
   },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'], // Ensure node_modules are not transformed
 };
 
 module.exports = createJestConfig(customJestConfig);
