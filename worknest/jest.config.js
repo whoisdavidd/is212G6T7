ccd276c938d98859f9549da8fa0a884da7460422
David <whoisdavidd@hotmail.com>
2024-10-24 16:42:50 +0800

    test

--- worknest/jest.config.js
+++ worknest/jest.config.js
@@ -13,8 +13,7 @@ const customJestConfig = {
   // Ensure Jest matches .test.mjs files
   testMatch: ['<rootDir>/src/app/test/*.test.mjs'],
   transform: {
-    // Use babel-jest to transform .mjs files
-    '^.+\\.mjs$': 'babel-jest',
+    '^.+\\.(js|jsx|mjs|ts|tsx)$': 'babel-jest', // Use babel-jest for .js, .jsx, .mjs files
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'], // Ensure node_modules are not transformed
 };
